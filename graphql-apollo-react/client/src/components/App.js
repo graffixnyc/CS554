e1705c3ddb7764718c4e84f7423b59689e6ae2ab
Patrick Hill <graffixnyc@gmail.com>
2019-04-15 14:56:11 -0400

    prettier test

--- graphql-apollo-react/client/src/components/App.js
+++ graphql-apollo-react/client/src/components/App.js
@@ -1,35 +1,35 @@
-import React, { Component } from 'react';
-import { NavLink, BrowserRouter as Router, Route } from 'react-router-dom';
+import React, {Component} from 'react';
+import {NavLink, BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 import Home from './Home';
 import Employees from './Employees';
 import Employers from './Employers';
 
 class App extends Component {
-	render() {
-		return (
-			<Router>
-				<header className='App-header'>
-					<h1 className='App-title'>GraphQL With Apollo</h1>
-					<nav>
-						<NavLink className='navlink' to='/'>
-							Home
-						</NavLink>
-						<NavLink className='navlink' to='/employees'>
-							Employees
-						</NavLink>
+  render() {
+    return (
+      <Router>
+        <header className='App-header'>
+          <h1 className='App-title'>GraphQL With Apollo</h1>
+          <nav>
+            <NavLink className='navlink' to='/'>
+              Home
+            </NavLink>
+            <NavLink className='navlink' to='/employees'>
+              Employees
+            </NavLink>
 
-						<NavLink className='navlink' to='/employers'>
-							Employers
-						</NavLink>
-					</nav>
-				</header>
-				<Route exact path='/' component={Home} />
-				<Route path='/employees/' component={Employees} />
-				<Route path='/employers/' component={Employers} />
-			</Router>
-		);
-	}
+            <NavLink className='navlink' to='/employers'>
+              Employers
+            </NavLink>
+          </nav>
+        </header>
+        <Route exact path='/' component={Home} />
+        <Route path='/employees/' component={Employees} />
+        <Route path='/employers/' component={Employers} />
+      </Router>
+    );
+  }
 }
 
 export default App;
--- graphql-apollo-react/client/src/components/Employers.js
+++ graphql-apollo-react/client/src/components/Employers.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 //Import Query from react-apollo
-import { Query } from 'react-apollo';
+import {Query} from 'react-apollo';
 
 //Import the Modals for Adding and Updating Employee
 import AddModal from './modals/AddModal';
@@ -13,80 +13,76 @@ That’s the important part: it executes the query when it is rendered.
 It uses React’s render props pattern, using a child as a function implementation where you can access the result of the query as an argument.
 */
 class Employers extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			showAddModal: false
-		};
-		//this.handleOpenEditModal = this.handleOpenEditModal.bind(this);
-		this.handleOpenAddModal = this.handleOpenAddModal.bind(this);
-		this.handleCloseModals = this.handleCloseModals.bind(this);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      showAddModal: false
+    };
+    //this.handleOpenEditModal = this.handleOpenEditModal.bind(this);
+    this.handleOpenAddModal = this.handleOpenAddModal.bind(this);
+    this.handleCloseModals = this.handleCloseModals.bind(this);
+  }
 
-	handleCloseModals() {
-		this.setState({ showAddModal: false });
-	}
+  handleCloseModals() {
+    this.setState({showAddModal: false});
+  }
 
-	handleOpenAddModal() {
-		this.setState({ showAddModal: true });
-	}
+  handleOpenAddModal() {
+    this.setState({showAddModal: true});
+  }
 
-	render() {
-		return (
-			<div>
-				<button className='button' onClick={this.handleOpenAddModal}>
-					Create Employer
-				</button>
-				<br />
-				<br />
-				<Query query={queries.GET_EMPLOYERS_WITH_EMPLOYEES} fetchPolicy={'cache-and-network'}>
-					{({ data }) => {
-						const { employers } = data;
-						if (!employers) {
-							return null;
-						}
-						return (
-							<div>
-								{employers.map((employer) => {
-									return (
-										<div className='card' key={employer.id}>
-											<div className='card-body'>
-												<h5 className='card-title'>{employer.name}</h5>
-												<span>Number of Employees:</span> {employer.numOfEmployees}
-												<br />
-												<br />
-												<span>Employees:</span>
-												<br />
-												<ol>
-													{employer.employees.map((employee) => {
-														return (
-															<li key={employee.id}>
-																{employee.firstName} {employee.lastName}
-															</li>
-														);
-													})}
-												</ol>
-											</div>
-										</div>
-									);
-								})}
-							</div>
-						);
-					}}
-				</Query>
+  render() {
+    return (
+      <div>
+        <button className='button' onClick={this.handleOpenAddModal}>
+          Create Employer
+        </button>
+        <br />
+        <br />
+        <Query query={queries.GET_EMPLOYERS_WITH_EMPLOYEES} fetchPolicy={'cache-and-network'}>
+          {({data}) => {
+            const {employers} = data;
+            if (!employers) {
+              return null;
+            }
+            return (
+              <div>
+                {employers.map((employer) => {
+                  return (
+                    <div className='card' key={employer.id}>
+                      <div className='card-body'>
+                        <h5 className='card-title'>{employer.name}</h5>
+                        <span>Number of Employees:</span> {employer.numOfEmployees}
+                        <br />
+                        <br />
+                        <span>Employees:</span>
+                        <br />
+                        <ol>
+                          {employer.employees.map((employee) => {
+                            return (
+                              <li key={employee.id}>
+                                {employee.firstName} {employee.lastName}
+                              </li>
+                            );
+                          })}
+                        </ol>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            );
+          }}
+        </Query>
 
-				{/*Add Employer Modal */}
-				{this.state &&
-				this.state.showAddModal && (
-					<AddModal
-						isOpen={this.state.showAddModal}
-						handleClose={this.handleCloseModals}
-						modal='addEmployer'
-					/>
-				)}
-			</div>
-		);
-	}
+        {/*Add Employer Modal */}
+        {this.state &&
+        this.state.showAddModal && (
+          <AddModal isOpen={this.state.showAddModal} handleClose={this.handleCloseModals} modal='addEmployer' />
+        )}
+      </div>
+    );
+  }
 }
 
 export default Employers;
