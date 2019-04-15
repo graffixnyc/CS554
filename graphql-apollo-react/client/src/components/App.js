7d5e00327ffcf1c137cbf77c9f7f5b7de2df8a29
Patrick Hill <graffixnyc@gmail.com>
2019-04-15 14:58:47 -0400

    prettier test

--- graphql-apollo-react/client/src/components/App.js
+++ graphql-apollo-react/client/src/components/App.js
@@ -6,30 +6,30 @@ import Employees from './Employees';
 import Employers from './Employers';
 
 class App extends Component {
-  render() {
-    return (
-      <Router>
-        <header className='App-header'>
-          <h1 className='App-title'>GraphQL With Apollo</h1>
-          <nav>
-            <NavLink className='navlink' to='/'>
-              Home
-            </NavLink>
-            <NavLink className='navlink' to='/employees'>
-              Employees
-            </NavLink>
+	render() {
+		return (
+			<Router>
+				<header className='App-header'>
+					<h1 className='App-title'>GraphQL With Apollo</h1>
+					<nav>
+						<NavLink className='navlink' to='/'>
+							Home
+						</NavLink>
+						<NavLink className='navlink' to='/employees'>
+							Employees
+						</NavLink>
 
-            <NavLink className='navlink' to='/employers'>
-              Employers
-            </NavLink>
-          </nav>
-        </header>
-        <Route exact path='/' component={Home} />
-        <Route path='/employees/' component={Employees} />
-        <Route path='/employers/' component={Employers} />
-      </Router>
-    );
-  }
+						<NavLink className='navlink' to='/employers'>
+							Employers
+						</NavLink>
+					</nav>
+				</header>
+				<Route exact path='/' component={Home} />
+				<Route path='/employees/' component={Employees} />
+				<Route path='/employers/' component={Employers} />
+			</Router>
+		);
+	}
 }
 
 export default App;
--- graphql-apollo-react/client/src/components/Employees.js
+++ graphql-apollo-react/client/src/components/Employees.js
@@ -15,116 +15,114 @@ That’s the important part: it executes the query when it is rendered.
 It uses React’s render props pattern, using a child as a function implementation where you can access the result of the query as an argument.
 */
 class Employees extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showEditModal: false,
-      showAddModal: false,
-      showDeleteModal: false
-    };
-    this.handleOpenEditModal = this.handleOpenEditModal.bind(this);
-    this.handleOpenAddModal = this.handleOpenAddModal.bind(this);
-    this.handleCloseModals = this.handleCloseModals.bind(this);
-  }
-  handleOpenEditModal(employee) {
-    this.setState({
-      showEditModal: true,
-      editEmployee: employee
-    });
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			showEditModal: false,
+			showAddModal: false,
+			showDeleteModal: false
+		};
+		this.handleOpenEditModal = this.handleOpenEditModal.bind(this);
+		this.handleOpenAddModal = this.handleOpenAddModal.bind(this);
+		this.handleCloseModals = this.handleCloseModals.bind(this);
+	}
+	handleOpenEditModal(employee) {
+		this.setState({
+			showEditModal: true,
+			editEmployee: employee
+		});
+	}
 
-  handleOpenDeleteModal(employee) {
-    this.setState({
-      showDeleteModal: true,
-      deleteEmployee: employee
-    });
-  }
-  handleCloseModals() {
-    this.setState({showAddModal: false, showEditModal: false, showDeleteModal: false});
-  }
+	handleOpenDeleteModal(employee) {
+		this.setState({
+			showDeleteModal: true,
+			deleteEmployee: employee
+		});
+	}
+	handleCloseModals() {
+		this.setState({showAddModal: false, showEditModal: false, showDeleteModal: false});
+	}
 
-  handleOpenAddModal() {
-    this.setState({showAddModal: true});
-  }
-  render() {
-    return (
-      <div>
-        <button className='button' onClick={this.handleOpenAddModal}>
-          Create Employee
-        </button>
-        <br />
-        <br />
-        <Query query={queries.GET_EMPLOYEES}>
-          {({data}) => {
-            const {employees} = data;
-            if (!employees) {
-              return null;
-            }
-            return (
-              <div>
-                {employees.map((employee) => {
-                  return (
-                    <div className='card' key={employee.id}>
-                      <div className='card-body'>
-                        <h5 className='card-title'>
-                          {employee.firstName} {employee.lastName}
-                        </h5>
-                        Employer: {employee.employer.name}
-                        <br />
-                        <button
-                          className='button'
-                          onClick={() => {
-                            this.handleOpenEditModal(employee);
-                          }}
-                        >
-                          Edit
-                        </button>
-                        <button
-                          className='button'
-                          onClick={() => {
-                            this.handleOpenDeleteModal(employee);
-                          }}
-                        >
-                          Delete
-                        </button>
-                        <br />
-                      </div>
-                    </div>
-                  );
-                })}
-              </div>
-            );
-          }}
-        </Query>
+	handleOpenAddModal() {
+		this.setState({showAddModal: true});
+	}
+	render() {
+		return (
+			<div>
+				<button className='button' onClick={this.handleOpenAddModal}>
+					Create Employee
+				</button>
+				<br />
+				<br />
+				<Query query={queries.GET_EMPLOYEES}>
+					{({data}) => {
+						const {employees} = data;
+						if (!employees) {
+							return null;
+						}
+						return (
+							<div>
+								{employees.map((employee) => {
+									return (
+										<div className='card' key={employee.id}>
+											<div className='card-body'>
+												<h5 className='card-title'>
+													{employee.firstName} {employee.lastName}
+												</h5>
+												Employer: {employee.employer.name}
+												<br />
+												<button
+													className='button'
+													onClick={() => {
+														this.handleOpenEditModal(employee);
+													}}>
+													Edit
+												</button>
+												<button
+													className='button'
+													onClick={() => {
+														this.handleOpenDeleteModal(employee);
+													}}>
+													Delete
+												</button>
+												<br />
+											</div>
+										</div>
+									);
+								})}
+							</div>
+						);
+					}}
+				</Query>
 
-        {/*Edit Employee Modal - NOT DONE YET */}
-        {this.state &&
-        this.state.showEditModal && (
-          <EditEmployeeModal
-            isOpen={this.state.showEditModal}
-            employee={this.state.editEmployee}
-            handleClose={this.handleCloseModals}
-          />
-        )}
+				{/*Edit Employee Modal - NOT DONE YET */}
+				{this.state &&
+				this.state.showEditModal && (
+					<EditEmployeeModal
+						isOpen={this.state.showEditModal}
+						employee={this.state.editEmployee}
+						handleClose={this.handleCloseModals}
+					/>
+				)}
 
-        {/*Add Employee Modal */}
-        {this.state &&
-        this.state.showAddModal && (
-          <AddModal isOpen={this.state.showAddModal} handleClose={this.handleCloseModals} modal='addEmployee' />
-        )}
+				{/*Add Employee Modal */}
+				{this.state &&
+				this.state.showAddModal && (
+					<AddModal isOpen={this.state.showAddModal} handleClose={this.handleCloseModals} modal='addEmployee' />
+				)}
 
-        {/*Add Employee Modal */}
-        {this.state &&
-        this.state.showDeleteModal && (
-          <DeleteEmployeeModal
-            isOpen={this.state.showDeleteModal}
-            handleClose={this.handleCloseModals}
-            deleteEmployee={this.state.deleteEmployee}
-          />
-        )}
-      </div>
-    );
-  }
+				{/*Add Employee Modal */}
+				{this.state &&
+				this.state.showDeleteModal && (
+					<DeleteEmployeeModal
+						isOpen={this.state.showDeleteModal}
+						handleClose={this.handleCloseModals}
+						deleteEmployee={this.state.deleteEmployee}
+					/>
+				)}
+			</div>
+		);
+	}
 }
 
 export default Employees;
