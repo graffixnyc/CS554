2f24280033a6bfe0aaa47bad21363ba6afe797b1
Patrick Hill <graffixnyc@gmail.com>
2019-04-15 15:22:10 -0400

    prettier test

--- graphql-apollo-react/client/src/components/modals/EditEmployeeModal.js
+++ graphql-apollo-react/client/src/components/modals/EditEmployeeModal.js
@@ -9,17 +9,17 @@ import queries from '../../queries';
 //For react-modal
 ReactModal.setAppElement('#root');
 const customStyles = {
-	content: {
-		top: '50%',
-		left: '50%',
-		right: 'auto',
-		bottom: 'auto',
-		marginRight: '-50%',
-		transform: 'translate(-50%, -50%)',
-		width: '50%',
-		border: '1px solid #28547a',
-		borderRadius: '4px'
-	}
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    width: '50%',
+    border: '1px solid #28547a',
+    borderRadius: '4px'
+  }
 };
 
 /* The React Apollo package grants access to a Query component, which takes a query as prop and executes it when its rendered. 
@@ -27,36 +27,36 @@ That’s the important part: it executes the query when it is rendered.
 It uses React’s render props pattern, using a child as a function implementation where you can access the result of the query as an argument.
 */
 class EditEmployeesModal extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			showEditModal: this.props.isOpen
-		};
-		this.handleCloseEditModal = this.handleCloseEditModal.bind(this);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      showEditModal: this.props.isOpen
+    };
+    this.handleCloseEditModal = this.handleCloseEditModal.bind(this);
+  }
 
-	handleCloseEditModal() {
-		this.setState({showEditModal: false});
-		this.props.handleClose();
-	}
+  handleCloseEditModal() {
+    this.setState({showEditModal: false});
+    this.props.handleClose();
+  }
 
-	render() {
-		return (
-			<div>
-				{/*Edit Employee Modal - NOT DONE YET */}
-				<ReactModal
-					name='editModal'
-					isOpen={this.state.showEditModal}
-					contentLabel='Edit Employee'
-					style={customStyles}>
-					<p>TODO - </p>
-					<button className='button cancel-button' onClick={this.handleCloseEditModal}>
-						Cancel
-					</button>
-				</ReactModal>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        {/*Edit Employee Modal - NOT DONE YET */}
+        <ReactModal
+          name='editModal'
+          isOpen={this.state.showEditModal}
+          contentLabel='Edit Employee'
+          style={customStyles}>
+          <p>TODO - </p>
+          <button className='button cancel-button' onClick={this.handleCloseEditModal}>
+            Cancel
+          </button>
+        </ReactModal>
+      </div>
+    );
+  }
 }
 
 export default EditEmployeesModal;
