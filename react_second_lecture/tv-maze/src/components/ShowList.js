c4f64c656af1113149e4b998e55337070e6cb1ed
Patrick Hill <graffixnyc@gmail.com>
2020-03-04 22:32:28 -0500

    Updated TV Maze app

--- react_second_lecture/tv-maze/src/components/ShowList.js
+++ react_second_lecture/tv-maze/src/components/ShowList.js
@@ -1,90 +1,92 @@
-import React, { Component } from "react";
-import axios from "axios";
-import { Link } from "react-router-dom";
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import SearchShows from './SearchShows';
+import noImage from '../img/download.jpeg';
 
 class ShowList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: undefined,
-      loading: false,
-      searchTerm: undefined,
-      searchData: undefined
-    };
-  }
-  async getShows() {
-    try {
-      const response = await axios.get("http://api.tvmaze.com/shows");
-      this.setState({ data: response.data });
-    } catch (e) {
-      console.log(e);
-    }
-  }
-  componentDidMount() {
-    this.getShows();
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			data: undefined,
+			searchTerm: undefined,
+			searchData: undefined
+		};
+	}
+	async getShows() {
+		if (this.state.searchTerm) {
+			try {
+				const { data } = await axios.get('http://api.tvmaze.com/search/shows?q=' + this.state.searchTerm);
+				this.setState({ searchData: data });
+			} catch (e) {
+				console.log(e);
+			}
+		} else {
+			try {
+				const { data } = await axios.get('http://api.tvmaze.com/shows');
+				this.setState({ data: data }, () => {
+					console.log(this.state.data);
+				});
+			} catch (e) {
+				console.log(e);
+			}
+		}
+	}
+	componentDidMount() {
+		this.getShows();
+	}
 
-  handleChange = e => {
-    let value = e.target.value;
-    this.setState({ searchTerm: value }, () => {
-      this.searchShows();
-    });
-  };
+	searchValue = (value) => {
+		this.setState({ searchTerm: value }, () => {
+			this.getShows();
+		});
+	};
 
-  onSubmit(e) {
-    e.preventDefault();
-  }
-  async searchShows() {
-    if (this.state.searchTerm) {
-      try {
-        const response = await axios.get(
-          "http://api.tvmaze.com/search/shows?q=" + this.state.searchTerm
-        );
-        this.setState({ searchData: response.data });
-      } catch (e) {
-        console.log(e);
-      }
-    }
-  }
-  render() {
-    let body = null;
-    let li = null;
-    if (this.state.searchTerm) {
-      li =
-        this.state.searchData &&
-        this.state.searchData.map(shows => {
-          let show = shows.show;
+	render() {
+		let body = null;
+		let li = null;
+		if (this.state.searchTerm) {
+			li =
+				this.state.searchData &&
+				this.state.searchData.map((shows) => {
+					let show = shows.show;
+					let img = null;
+					if (show.image) {
+						img = <img alt='Show' src={show.image.medium} />;
+						<br />;
+					} else {
+						img = <img alt='Show' src={noImage} />;
+					}
+					return (
+						<li key={show.id}>
+							<Link to={`/shows/${show.id}`}>
+								{img} <br />
+								{show.name}
+							</Link>
+						</li>
+					);
+				});
+		} else {
+			li =
+				this.state.data &&
+				this.state.data.map((show) => (
+					<li key={show.id}>
+						<Link to={`/shows/${show.id}`}>
+							<img alt='Show' src={show.image.medium} />; <br />
+							{show.name}
+						</Link>
+					</li>
+				));
+		}
+		body = (
+			<div>
+				<SearchShows searchValue={this.searchValue} />
+				<ul className='list-unstyled'>{li}</ul>
+			</div>
+		);
 
-          return (
-            <li key={show.id}>
-              <Link to={`/shows/${show.id}`}>{show.name}</Link>
-            </li>
-          );
-        });
-    } else {
-      li =
-        this.state.data &&
-        this.state.data.map(show => (
-          <li key={show.id}>
-            <Link to={`/shows/${show.id}`}>{show.name}</Link>
-          </li>
-        ));
-    }
-    body = (
-      <div>
-        <form method="POST " name="formName" onSubmit={this.onSubmit}>
-          <label>
-            {" "}
-            Search Term:
-            <input type="text" name="searchTerm" onChange={this.handleChange} />
-          </label>
-        </form>
-        <ul className="list-unstyled">{li}</ul>
-      </div>
-    );
-
-    return body;
-  }
+		return body;
+	}
 }
 
 export default ShowList;
