8cab25d05491f9fc6b5671aae762778d90672acd
Patrick Hill <graffixnyc@gmail.com>
2020-09-09 17:03:12 -0400

    working on updated version of TV Maze

--- react_second_lecture/tv-maze-function-components/src/App.js
+++ react_second_lecture/tv-maze-function-components/src/App.js
@@ -3,29 +3,33 @@ import logo from './img/tvm-header-logo.png';
 import './App.css';
 import ShowList from './components/ShowList';
 import Show from './components/Show';
+import Home from './components/Home';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 const App = () => {
-  return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to the TV Maze API</h1>
-          <Link className="showlink" to="/shows">
-            Shows
-          </Link>
-        </header>
-        <br />
-        <br />
-        <div className="App-body">
-          <p>Welcome to the TV Maze API example</p>
-          <Route path="/shows" exact component={ShowList} />
-          <Route path="/shows/:id" exact component={Show} />
-        </div>
-      </div>
-    </Router>
-  );
+	return (
+		<Router>
+			<div className='App'>
+				<header className='App-header'>
+					<img src={logo} className='App-logo' alt='logo' />
+					<h1 className='App-title'>Welcome to the React.js TV Maze API Example</h1>
+					<Link className='showlink' to='/'>
+						Home
+					</Link>
+					<Link className='showlink' to='/shows'>
+						Shows
+					</Link>
+				</header>
+				<br />
+				<br />
+				<div className='App-body'>
+					<Route exact path='/' component={Home} />
+					<Route exact path='/shows' component={ShowList} />
+					<Route exact path='/shows/:id' component={Show} />
+				</div>
+			</div>
+		</Router>
+	);
 };
 
 export default App;
